4ebbf3b5166ba588b40c64d6c13856d00ec2eacf
allinthemind66 <r.nilon92@gmail.com>
2018-01-30 16:05:28 -0500

    wrote method

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,16 @@
+require 'pry'
 def count_elements(array)
+  hash = {}
+  array.each do |item|
+    #
+    if hash[item]
+      hash[item] +=1
+      #binding.pry
+    else
+      hash[item] = 1
+    end
+
+  end
   # code goes here
+  hash
 end
- 
